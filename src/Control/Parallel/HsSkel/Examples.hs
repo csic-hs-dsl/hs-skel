1cb69e50e88fa33a173d878adc58fbce0c26a6df
Pablo Perdomo <pperdomo@despegar.com>
2015-02-01 18:13:27 -0200

    Mi versión del KMeans.

--- src/Control/Parallel/HsSkel/Examples.hs
+++ src/Control/Parallel/HsSkel/Examples.hs
@@ -19,7 +19,8 @@ import Prelude hiding (mapM, id, (.))
 import System.Random (randomRs)
 import System.Random.TF.Init (mkTFGen)
 
-import Data.List (groupBy, minimumBy)
+import Data.List (groupBy, minimumBy, find, sortBy)
+import Data.Maybe (fromJust)
 
 {- ========================================================= -}
 {- ======================== Utils ========================== -}
@@ -170,14 +171,19 @@ execSkKMeansOneStep = do
     let (mys, _) = splitAt k mxsRest
     let ps = zip pxs pys
     let ms = zip mxs mys
+    print "ps: "
     print ps
+    print "ms: "
     print ms
-    res <- exec skKMeansOneStep ((ps, ms), fromIntegral k)
+    resSk <- exec skKMeansOneStep ((ps, ms), fromIntegral k)
     print "fin"
-    print res
+    print resSk
 
-    print "calcNewMeans"
+    print "kMeansTestMauro"
     print $ kMeansTestMauro ps ms
+    
+    print "kMeansTestPablo"
+    kMeansTestPablo ps ms >>= print 
 
 kMeansTestMauro :: (Ord t, Floating t) => [(t, t)] -> [(t, t)] -> [(t, t)]
 kMeansTestMauro ps ms = 
@@ -196,3 +202,46 @@ kMeansTestMauro ps ms =
         calcNewMeans = map calcNewMean ms
     in calcNewMeans
 
+
+
+type Point = (Double, Double)
+type Mean = (Double, Double)
+type Distance = Double
+kMeansTestPablo :: [Point] -> [Mean] -> IO [Mean]
+kMeansTestPablo points means = do
+    let 
+        sqr :: Double -> Double
+        sqr a = a * a        
+        
+        fst3 (a, _, _) = a
+        snd3 (_, b, _) = b
+        trd3 (_, _, c) = c
+        
+        distance :: Point -> Point -> Distance
+        distance (a1, a2) (b1, b2) = sqrt (sqr (a1 - b1) + sqr (a2 - b2))
+        
+        distances :: [(Point, Mean, Distance)]
+        distances = concat $ map (\med -> [(x, med, distance x med) | x <- points]) means
+    
+        grouped :: [[(Point, Mean, Distance)]]
+        grouped = groupBy (\p1 p2 -> fst3 p1 == fst3 p2) $ sortBy (\p1 p2 -> compare (fst3 p1) (fst3 p2)) distances
+        
+        mins :: [(Point, Mean, Distance)]
+        mins = map (minimumBy (\p1 p2 -> compare (trd3 p1) (trd3 p2))) grouped
+        
+        groupedMeds :: [[(Point, Mean, Distance)]]
+        groupedMeds = groupBy (\p1 p2-> snd3 p1 == snd3 p2) $ sortBy (\p1 p2 -> compare (snd3 p1) (snd3 p2)) mins
+        
+        meds :: [(Mean, [Point])]
+        meds = map (\ls -> (snd3 $ head ls, map fst3 ls)) groupedMeds
+        
+        sumPair (a1, a2) (b1, b2) = (a1 + b1, a2 + b2)
+        dividePair (a1, a2) (b1, b2) = (a1 / b1, a2 / b2)
+        pair a = (a, a)
+        
+        newMeds :: [(Mean, Mean)]
+        newMeds = map (\(m, ls) -> (m, dividePair (foldl1 sumPair ls) (pair $ fromIntegral $ length ls))) meds
+
+        newMedsSorted :: [Mean]
+        newMedsSorted = map (\m -> snd $ fromJust $ find (\(m2, _) -> m == m2) newMeds) means
+    return newMedsSorted
