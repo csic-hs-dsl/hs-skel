6b4e5f56b1da4cdf8cb54076de2a8406a4759c43
Pablo Perdomo <pperdomo@despegar.com>
2015-05-18 18:00:53 -0300

    * Nuevas objetivo: copiar a Scala!

--- src/Control/Parallel/HsSkel/DSL.hs
+++ src/Control/Parallel/HsSkel/DSL.hs
@@ -7,13 +7,7 @@
 {-# LANGUAGE TypeOperators #-}
 {-# OPTIONS_HADDOCK show-extensions #-}
 
--- | This module defines the DSL to construct Streams and Skeletons.
-module Control.Parallel.HsSkel.DSL (
-    -- * Types
-    Skel(..),
-    -- * Execution
-    Exec(Context, exec),
-) where
+module Control.Parallel.HsSkel.DSL where
 
 import Data.Traversable --(Traversable)
 import Control.Arrow --(Arrow(arr, first, second, (***)), ArrowChoice(left, right, (+++)), ArrowApply(app), returnA)
@@ -36,8 +30,9 @@ import Control.Concurrent.MVar
 
 data Future i where
     Now :: i -> Future i
-    Later :: Future i -> (i -> Future o) -> Future o
+    Bind :: Future i -> (i -> Future o) -> Future o
 
+    Later :: (NFData i) => i -> Future i
 
 instance Functor Future where
     fmap = liftM
@@ -48,9 +43,9 @@ instance Applicative Future where
 
 instance Monad Future where
     return = Now
-    (>>=) = Later
+    (>>=) = Bind
 
-instance (Num a) => Num (Future a) where
+{- instance (Num a) => Num (Future a) where
     (+) f1 f2 = pure (+) <*> f1 <*> f2
     (-) f1 f2 = pure (-) <*> f1 <*> f2
     (*) f1 f2 = pure (*) <*> f1 <*> f2
@@ -58,123 +53,117 @@ instance (Num a) => Num (Future a) where
     abs = (return . abs =<<)
     signum = (return . signum =<<)
     fromInteger = Now . fromInteger
-
-data Skel i o where
-    SkLazy   :: (i -> o) -> Skel i o
-    SkComp   :: Skel x o -> Skel i x -> Skel i o
-    SkPair   :: Skel i1 o1 -> Skel i2 o2 -> Skel (i1, i2) (o1, o2)
-    SkChoice :: Skel i o -> Skel i' o' -> Skel (Either i i') (Either o o')
-    SkApply  :: Skel (Skel i o, i) o
-
-    SkStrict   :: (NFData o) => (i -> o) -> Skel i (Future o)
-    SkAndThen  :: (NFData o) => (i -> o) -> Skel (Future i) (Future o)
-    SkTuple    :: (NFData i1, NFData i2) => Skel (Future i1, Future i2) (Future (i1, i2))
-    SkTraverse :: (NFData i, Traversable t) => Skel (t (Future i)) (Future (t i))
+-}
 
 
-instance Category (Skel) where
-    id  = SkLazy id
-    (.) = SkComp
+data DSL i o where
+    Arr    :: (i -> o) -> DSL i o
+    Comp   :: DSL x o -> DSL i x -> DSL i o
+    Pair   :: DSL i1 o1 -> DSL i2 o2 -> DSL (i1, i2) (o1, o2)
 
-instance Arrow (Skel) where
-    arr = SkLazy
-    first sk = sk `SkPair` id
-    second sk = id `SkPair` sk
-    (***) = SkPair
+    Par  :: NFData o => (i -> o) -> DSL i (Future o)
+    Seq  :: (i -> o) -> DSL i (Future o)
+
+
+
+instance Category DSL where
+    id  = Arr id
+    (.) = Comp
+
+instance Arrow DSL where
+    arr = Arr
+    first sk = sk `Pair` id
+    second sk = id `Pair` sk
+    (***) = Pair
     --(&&&) sk1 sk2 = idem to default implementation.
 
-instance ArrowChoice (Skel) where
-    left sk = SkChoice sk id
-    right sk = SkChoice id sk
-    (+++) = SkChoice
-    --(|||) sk1 sk2 = idem to default implementation.
-
-instance ArrowApply (Skel) where
-    app = SkApply
 
 
-instance Show (Future i) where
-    show (Now _) = "Now"
-    show (Later f _) = "Later (" ++ show f ++ ")"
+{-
+successful :: o -> Future o
+successful o = Now o
 
-instance Show (Skel a b) where
-    show (SkLazy _) = "<SkLazy/>"
-    show (SkComp skel1 skel2) = "<SkComp> " ++ show skel1 ++ " " ++ show skel2 ++ "</SkComp>"
-    show (SkPair skel1 skel2) = "<SkPair> " ++ show skel1 ++ " " ++ show skel2 ++ "</SkPair>"
-    show (SkChoice skel1 skel2) = "<SkChoice> " ++ show skel1 ++ " " ++ show skel2 ++ "</SkChoice>"
-    show (SkApply) = "<SkApply/>"
-    show (SkStrict _) = "<SkStrict/>"
-    show (SkAndThen _) = "<SkAndThen/>"
-    show (SkTuple) = "<SkTuple/>"
-    show (SkTraverse) = "<SkTraverse/>"
+future :: NFData o => o -> Future o
+future o = Later o
 
-{- ================================================================== -}
-{- ======================= Execution Context ======================== -}
-{- ================================================================== -}
-
-class (Monad m) => Exec m where
-    type Context m :: *
-    exec :: Context m -> Skel i o -> i -> m o
-
-
-
-data IOContext = IOContext
-instance Exec IO where
-    type Context IO = IOContext
-    exec = execIO
-
-execIO :: IOContext -> Skel i o -> i -> IO o
-execIO ctx skel input = undefined
-
-
-sumacostosas :: (Int -> Int) -> (Int -> Int) -> Skel (Int, Int) (Future Int)
-sumacostosas c1 c2 = proc (i1, i2) -> do
-    f1 <- SkStrict c1 -< i1
-    f2 <- SkStrict c2 -< i2
-    ft <- SkTuple -< (f1, f2)
-    SkAndThen (uncurry (+)) -< ft
-
-sumacostosas2 :: (Int -> Int) -> (Int -> Int) -> (Int -> Int) -> Skel (Int, Int, Int) (Future Int)
-sumacostosas2 c1 c2 c3 = proc (i1, i2, i3) -> do
-    f1 <- SkStrict c1 -< i1
-    f2 <- SkStrict c2 -< i2
-    f3 <- SkStrict c3 -< i3
-    fl <- SkTraverse -< [f1, f2, f3]
-    SkAndThen sum -< fl
-
-
--- Esto es practicamente Scala
--- sumacostosas usando la mónada Future
-sumacostosas3 :: (Int -> Int) -> (Int -> Int) -> Skel (Int, Int) (Future Int)
-sumacostosas3 c1 c2 = proc (i1, i2) -> do
-    f1 <- SkStrict c1 -< i1
-    f2 <- SkStrict c2 -< i2
+sumacostosas1 :: Monad m => (Int -> Int) -> (Int -> Int) -> (Int, Int) -> m (Future Int)
+sumacostosas1 c1 c2 (i1, i2) = do
+    f1 <- return (future $ c1 i1)
+    f2 <- return (future $ c2 i2)
     let fr = do
         v1 <- f1
         v2 <- f2
-        return $ v1 + v2
-    returnA -< fr
+        successful $ v1 + v2
+    return fr
 
--- sumacostosas usando la mónada Future con fmap, uncurry y fTuple
-sumacostosas4 :: (Int -> Int) -> (Int -> Int) -> Skel (Int, Int) (Future Int)
-sumacostosas4 c1 c2 = proc (i1, i2) -> do
-    f1 <- SkStrict c1 -< i1
-    f2 <- SkStrict c2 -< i2
-    returnA -< pure (+) <*> f1 <*> f2
+sumacostosas2 :: Monad m => (Int -> Int) -> (Int -> Int) -> (Int, Int) -> m (Future Int)
+sumacostosas2 c1 c2 (i1, i2) = do
+    let fr = do
+        v1 <- future $ c1 i1
+        v2 <- future $ c2 i2
+        successful $ v1 + v2
+    return fr
 
--- sumacostosas usando la mónada Future con sequence
-sumacostosas5 :: (Int -> Int) -> (Int -> Int) -> Skel (Int, Int) (Future Int)
-sumacostosas5 c1 c2 = proc (i1, i2) -> do
-    f1 <- SkStrict c1 -< i1
-    f2 <- SkStrict c2 -< i2
-    returnA -< do
-        fr <- M.sequence $ [f1, f2]
-        return $ sum fr
+runPrint :: Future o -> IO o
+runPrint (Now o) = putStrLn "<Now/>" >> return o
+runPrint (Bind f fun) = do
+    putStrLn "<Bind>"
+    v <- runPrint f
+    o <- runPrint (fun v)
+    putStrLn "</Bind>"
+    return o
+runPrint (Later o) = putStrLn "<Later/>" >> return o
 
--- sumacostosas usando la mónada Future y su instancia de Num
-sumacostosas6 :: (Int -> Int) -> (Int -> Int) -> Skel (Int, Int) (Future Int)
-sumacostosas6 c1 c2 = proc (i1, i2) -> do
-    f1 <- SkStrict c1 -< i1
-    f2 <- SkStrict c2 -< i2
-    returnA -< f1 + f2
+run :: Future o -> IO o
+run f = do
+    putStrLn "<Future>"
+    r <- runPrint f
+    putStrLn "</Future>"
+    return r
+
+ej1 = do 
+    s <- sumacostosas1 (+1) (+2) (1,2)
+    run s
+
+-}
+
+
+successful :: (i -> o) -> DSL i (Future o)
+successful f = Seq f
+
+future :: NFData o => (i -> o) -> DSL i (Future o)
+future f = Par f
+
+sumacostosas1 :: (Int -> Int) -> (Int -> Int) -> DSL (Int, Int) (Future Int)
+sumacostosas1 c1 c2 = proc (i1, i2) -> do
+    f1 <- future c1 -< i1
+    f2 <- future c2 -< i2
+    let fr = do 
+            v1 <- f1
+            v2 <- f2
+            return $ v1 + v2
+    id -< fr
+
+
+sumacostosas1 :: (Int -> Int) -> (Int -> Int) -> DSL (Int, Int) (Future Int)
+sumacostosas1 c1 c2 = proc (i1, i2) -> do
+    f1 <- future c1 -< i1
+    f2 <- future c2 -< i2
+    let fr = do 
+            v1 <- f1
+            v2 <- f2
+            return $ v1 + v2
+    id -< fr
+
+
+sumacostosas2 :: (Int -> Int) -> (Int -> Int) -> DSL (Int, Int) (Future Int)
+sumacostosas2 c1 c2 = proc (i1, i2) -> do
+    let fr = do 
+            v1 <- Later (c1 i1)
+            v2 <- Later (c2 i2)
+            return $ v1 + v2
+    id -< fr
+
+run :: (NFData o, Monad m) => i -> DSL i (Future o) -> m o
+run i dsl = undefined
 
